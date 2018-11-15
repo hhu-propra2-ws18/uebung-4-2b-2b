c81bd88539bd8dc5f4fb9bf074a179208734c76d
commanderchristopher <christopher.orlowicz@hhu.de>
2018-11-15 14:50:27 +0100

    Add Auto definition

--- src/main/java/de/hhu/propra/db/entities/Auto.java
+++ src/main/java/de/hhu/propra/db/entities/Auto.java
@@ -1,5 +1,21 @@
 package de.hhu.propra.db.entities;
 
-public class Auto{
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Auto{
+    @Id
+    @GeneratedValue
+    long id;
+
+    private String marke;
+
+    private String farbe;
+
+    private Integer hubraum;
 }
